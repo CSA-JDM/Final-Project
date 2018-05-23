02f2092b9a0403b1f8b70219614489b48a254772
CSA-JDM <33459798+CSA-JDM@users.noreply.github.com>
2018-05-23 10:08:56 -0500

    Version 1.0
    First "official" version.

--- In Memoriam.py
+++ In Memoriam.py
@@ -2,27 +2,13 @@
 # 4th Period, Computer Programming
 # April 16th, 2018
 """
-This will be taking place over the next few weeks, with minor reviews put in place at the end of each week.
-You will also have to show me progress each week on the program.
-You are welcome to use research, but I will not be able to answer very specific questions
-(this is a review, I can't reteach you things you should already know).
-I need a proposal from you in regards what you want to do with Python.
-This will need to cover everything we have done this year (from simple printing to loops to classes and reading/writing
-files).
-Of course a common one is simple games, but ones that are useful in everyday life would be great too
-(maybe terminal programs to reserve airplanes or rental cars, inventory/cash register, etc).
-This proposal must be completed BEFORE class on Wednesday.
-You can ask me ideas for what to do, you can ask each other as well.
-Look online for ideas as well.
-Proposal should be professional, slide show with examples.
-Must be an in depth description of what you want to do, why, what it will do, and how it is a useful program.
-It will also need a time line of what you will have done and when.
+A simple RPG with the goal being to escape a monster-infested region.
 """
 import Personal_Modules.Canvas_Objects as Canvas_Objects
+import Personal_Modules.mp3play as mp3play
 import turtle
 import time
 import random
-import mp3play
 
 
 class App:
